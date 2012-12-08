9ae6e63e2e7e9b078c3cd89d5b885a94acdbe438
Kamal Micheal <kamal.micheal@bibalex.org>
2012-12-08 22:03:31 +0200

    Initial post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EolRegistryRails::Application.config.secret_token = '8b1490aa98b8816d5c0cfb76a44b61b19fb4dbc7e2d071730ef47a1c8e443e5c71fedfc33287fad5d879c8ae57b2435514c1ea7dd09951c01dac0abc9f9a3849'
